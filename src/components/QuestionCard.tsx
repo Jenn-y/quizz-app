18b494b91b61651e1a37641c66c506eaa96a392b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-05 23:34:25 +0100

    implemented question card component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -5,7 +5,7 @@ type Props = {
   question: string;
   answers: string[];
   callback: any;
-  userAnswer: string;
+  userAnswer: any;
   questionNum: number;
   totalQuestions: number;
 };
@@ -14,11 +14,23 @@ const QuestionCard = ({
   question,
   answers,
   callback,
+  questionNum,
+  totalQuestions,
   userAnswer
 }: Props) => (
   <>
+    <p className="number">Question: {questionNum}/{totalQuestions}</p>
     <p dangerouslySetInnerHTML={{ __html: question }}></p>
     <div>
+      {
+        answers.map(answer => {
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{__html: answer}} />
+            </button>
+          </div>
+        })
+      }
     </div>
   </>
 );
