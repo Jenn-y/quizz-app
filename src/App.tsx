5ab380b4c56db7206b6a266ccc804f814e03c9ef
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-11 21:12:10 +0100

    check correct answer

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,20 @@ const App = () => {
   };
 
   const checkAnswer = (e: MouseEvent) => {
-
+    if (!gameover){
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if (correct){
+        setScore(prev => prev + 1);
+      }
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer 
+      };
+      setAnswers((prev) => [...prev, answerObject]);
+    }
   };
 
   const nextQuestion = () => {
